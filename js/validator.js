8cc6d903999d863847b4e62fbdd1ba4c8c6ad575
Jacob Nuno <jacobcnda@gmail.com>
2018-11-28 18:43:37 -0600

    css has-error validator

--- js/validator.js
+++ js/validator.js
@@ -1,115 +1,78 @@
-/**
-  * [Validator-
-  *The class validates and specifies the syntax rules of the data]
-*/
-class Validator {
-    /**
-    * [word Regular expression for the words introduced for the user ]
-    * @type {[varchar]}
-    * [email Regular expression for add email in data]
-    * @type {[varchar]}
-    */
-    static get regex() {
-        return {
-            word: /[a-zA-ZñÑ ]{3,}/,
-            email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            number: /^\d+$/,
-            decimal: /^(\d+\.?\d{0,4}|\.\d{1,4})$/,
-            date: /^[1-2][0-9]{3}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-2])$/,
-            hour: /^([01][0-9]|2[0-3]:[0-5][0-9])$/,
-        };
+const buttonSubmit = document.querySelector('button[data-type=submit]');
+const form = buttonSubmit.parentNode;
+const inputs = form.getElementsByTagName('input');
+
+buttonSubmit.onclick = function () {
+  for (const input of inputs) {
+    let rule = input.getAttribute('data-rule');
+    console.log('rule: ', rule);
+    if(rule != 'radio' && rule != 'image') {
+      hasError(input);
+      window[rule](input);
     }
+  }
+};
 
-    /**
-     * ["word" Fuction for validated word in data ]
-     * @param  {[Object]} data [Any word inside API to add or updated regiters]
-     * @return {[Fuction]}      [Fuction to test regular expression inside the words ]
-    */
-    // static word(data) {
-    //     return (Validator.regex.word.test(data));
-    // }
+const regex = {
+    // word: /[a-zA-ZñÑ ]{3,}/,
+    word: /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/,
+    email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    number: /^\d+$/,
+    decimal: /^(\d+\.?\d{0,4}|\.\d{1,4})$/,
+    date: /^[1-2][0-9]{3}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-2])$/,
+    hour: /^([01][0-9]|2[0-3]:[0-5][0-9])$/,
+};
 
-    static word(input) {
-        if(this.regex.word.test(input.value)) {
-            this.addError(input);
-        }
-    }
+function getError(input) {
+    const inputRule = input.getAttribute('data-rule');
+    return `The field should be a valid ${inputRule}`;
+}
 
-    static number(data) {
-        return (Validator.regex.number.test(data));
-    }
+function resetFormGroup(input) {
+    // Remove the success and error classes
+    input.parentNode.classList.remove('has-error');
+    // and remove span error
+    input.parentNode.removeChild(input.parentNode.querySelector('span.has-error'));
+}
 
-    static decimal(data) {
-        return (Validator.regex.decimal.test(data));
-    }
+function addError(input) {
+    const divParent = input.parentNode;
+    const newSpan = document.createElement('span');
+    newSpan.appendChild(document.createTextNode(getError(input)));
+    newSpan.classList.add('has-error');
+    divParent.appendChild(newSpan);
+    divParent.classList.add('has-error');
+}
 
-    static date(data) {
-        return (Validator.regex.date.test(data));
-    }
-
-    static hour(data) {
-        return (Validator.regex.hour.test(data));
-    }
-
-    static image(data) {
-        return data === 'image/jpeg';
-    }
-
-    /**
-     * ["required" fuction for validated when data introduced its empty or incorrect]
-     * @param  {[Object]} data [Data introduced by user ]
-     * @return {[Fuction]}      [Fuction for compare data with other cases]
-    */
-    static required(data) {
-        return data !== undefined && data !== null && data.length;
-    }
-
-    /**
-     * ["email" Fuction to Valited regular expression to entry data to e-mail ]
-     * @param  {[Object]} data [Data introduced by user, to validate]
-     * @return {[Fuction]}      [Fuction to validate the rules with regular expression]
-    */
-    static email(data) {
-        return (Validator.regex.email.test(data));
-    }
-
-    /**
-     * [validate Fuction general to validate with diferent rules and details]
-     * @param  {[Object]}   rules [validate with especific rule in especial case of data]
-     * @return {[message]}         [Details to validate the mistake ]
-    */
-    // static validate(rules) {
-    //     const errors = [];
-    //
-    //     for (let part in rules) {
-    //         for (let field in rules[part]) {
-    //             let validators = rules[part][field].split(',');
-    //                 validators.forEach((f) => {
-    //                 if (!Validator[f](req[part][field] || '')) {
-    //                     // if (Array.isArray(error.details[field])) {
-    //                     //   error.details[field].push(`The field ${field} should be a valid ${f}`);
-    //                     // } else {
-    //                     //   error.details[field] = [`The field ${field} should be a valid ${f}`];
-    //                     // }
-    //                     errors.push({ error: `The field ${field} should be a valid ${f}` });
-    //                 }
-    //             });
-    //         }
-    //     }
-    //     return errors.length ? errors : 0;
-    // }
-
-    static getError(input) {
-        const inputRule = input.getAttribute('data-rule');
-        return `The field should be a valid ${inputRule}`;
-    }
-
-    static addError(input) {
-        const divParent = input.parentNode;
-        const block = divParent.createElement('span');
-        block.innerHTML = this.getError(input);
-        divParent.classList.add('has-error');
+function hasError(input) {
+    const divParent = input.parentNode;
+    if (divParent.querySelector('span.has-error')) {
+        resetFormGroup(input);
     }
 }
 
-module.exports = Validator;
+function word(input) {
+    if (!regex.word.test(input.value)) {
+        addError(input);
+    }
+}
+
+function number(data) {
+    return (this.regex.number.test(data));
+}
+
+function decimal(data) {
+    return (this.regex.decimal.test(data));
+}
+
+function date(data) {
+    return (this.regex.date.test(data));
+}
+
+function hour(data) {
+    return (this.regex.hour.test(data));
+}
+
+function email(data) {
+    return (this.regex.email.test(data));
+}
