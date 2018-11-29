9f720a2b2a10fe61a196371139b0e7202099ef9a
Jacob Nuno <jacobcnda@gmail.com>
2018-11-28 18:39:35 -0600

    validator

--- js/validateForm.js
+++ js/validateForm.js
@@ -1,25 +1,59 @@
-const { validator } = require('./index');
-
 const buttonSubmit = document.querySelector('button[data-type=submit]');
-console.log('buttonSubmit', buttonSubmit);
 const form = buttonSubmit.parentNode;
-console.log('form', form);
 const inputs = form.getElementsByTagName('input');
-console.log('inputs', inputs);
 
 buttonSubmit.onclick = function () {
   for (const input of inputs) {
     let rule = input.getAttribute('data-rule');
     console.log('rule: ', rule);
     if(rule != 'radio' && rule != 'image') {
-      validator.rule(input);
+      hasError(input);
+      window[rule](input);
     }
   }
 };
 
+const regex = {
+    // word: /[a-zA-ZñÑ ]{3,}/,
+    word: /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/,
+    email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    number: /^\d+$/,
+    decimal: /^(\d+\.?\d{0,4}|\.\d{1,4})$/,
+    date: /^[1-2][0-9]{3}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-2])$/,
+    hour: /^([01][0-9]|2[0-3]:[0-5][0-9])$/,
+};
+
+function getError(input) {
+    const inputRule = input.getAttribute('data-rule');
+    return `The field should be a valid ${inputRule}`;
+}
+
+function resetFormGroup(input) {
+    // Remove the success and error classes
+    input.parentNode.classList.remove('has-error');
+    // and remove span error
+    input.parentNode.removeChild(input.parentNode.querySelector('span.has-error'));
+}
+
+function addError(input) {
+    const divParent = input.parentNode;
+    const newSpan = document.createElement('span');
+    newSpan.appendChild(document.createTextNode(getError(input)));
+    newSpan.classList.add('has-error');
+    divParent.appendChild(newSpan);
+    divParent.classList.add('has-error');
+}
+
+function hasError(input) {
+    const divParent = input.parentNode;
+    if (divParent.querySelector('span.has-error')) {
+        resetFormGroup(input);
+    }
+}
+
 function word(input) {
-    if(this.regex.word.test(input.value)) {
-      addError(input);
+    if (!regex.word.test(input.value)) {
+        addError(input);
     }
 }
 
@@ -41,36 +75,4 @@ function hour(data) {
 
 function email(data) {
     return (this.regex.email.test(data));
-}
-
-// Shows the errors for a specific input
-  // function showErrorsForInput(input, errors) {
-  //   // This is the root of the input
-  //   const formGroup = closestParent(input.parentNode, "form-group");
-  //   // First we remove any old messages and resets the classes
-  //   resetFormGroup(formGroup);
-  //   // If we have errors
-  //   if (errors) {
-  //     // we first mark the group has having errors
-  //     formGroup.classList.add("has-error");
-  //     // then we append all the errors
-  //     _.each(errors, function(error) {
-  //       addError(messages, error);
-  //     });
-  //   } else {
-  //     // otherwise we simply mark it as success
-  //     formGroup.classList.add("has-success");
-  //   }
-  // }
-
-function getError(input) {
-  let inputRule = input.getAttribute('data-rule');
-  return `The field should be a valid ${inputRule}`;
-}
-
-function addError(input, error) {
-    const divParent = input.parentNode;
-    let block = divParent.createElement('span');
-    block.innerHTML = this.getError(input);
-    divParent.classList.add('has-error');
 }
