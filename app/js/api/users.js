e70a116ac89c19fc34ed6c4194fe5077ddd4be5a
Jacob Nuno <jacobcnda@gmail.com>
2018-12-01 16:24:43 -0600

    users api

---
+++ app/js/api/users.js
@@ -0,0 +1,36 @@
+const cookies = require('../cookies');
+
+class Users {
+  constructor() {
+    const this.endpoint = 'character';
+    const this.apiUrl = ;
+
+    // obtener la cookie
+    // document.cookie
+    // this.token = asdfasdf
+
+    // setear el token
+    this.headers = new Headers();
+    this.headers.set('Authorization', 'Bearer' + this.token)
+  }
+
+
+  function getAll(x) {
+    return await fetch(`${this.apiUrl}/${this.endpoint}/asdfasd`, this.headers).then(function(response) {
+      return response.json();
+    });
+  }
+
+  function create(form) {
+    data = await fetch(`${this.apiUrl}/${this.endpoint}`, {
+      method: 'post',
+      body: new FormData(form)
+    }).then(function(response){
+
+    })
+
+    document.cookie = 'credentials' + "=" + data.token + ";path=/;expires=" + d.toGMTString();
+  }
+}
+
+module.exports = new Users();
