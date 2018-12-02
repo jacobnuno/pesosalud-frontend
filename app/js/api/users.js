4c14123b154ce30ebe1a2fa9baf7f1dc81a46124
Jacob Nuno <jacobcnda@gmail.com>
2018-12-01 22:14:08 -0600

    avance

--- app/js/api/users.js
+++ app/js/api/users.js
@@ -2,8 +2,7 @@ const cookies = require('../cookies');
 
 class Users {
   constructor() {
-    const this.endpoint = 'character';
-    const this.apiUrl = ;
+    const this.apiUrl = '';
 
     // obtener la cookie
     // document.cookie
@@ -11,12 +10,31 @@ class Users {
 
     // setear el token
     this.headers = new Headers();
-    this.headers.set('Authorization', 'Bearer' + this.token)
   }
 
+  function login() {
+    return await fetch(`${this.apiUrl}/${this.endpoint}/asdfasd`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: JSON.stringify(data),
+    }).then(function(response) {
 
-  function getAll(x) {
-    return await fetch(`${this.apiUrl}/${this.endpoint}/asdfasd`, this.headers).then(function(response) {
+      this.headers.set('Authorization', 'Bearer' + this.token);
+      return response.json();
+    });
+
+  }
+
+  function getAll() {
+    return await fetch(`${this.apiUrl}/${this.endpoint}/asdfasd`, {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer' + this.token,
+        "Content-Type": "application/json"
+      }
+    }).then(function(response) {
       return response.json();
     });
   }
