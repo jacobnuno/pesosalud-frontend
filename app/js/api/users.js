37bede6e31aaf30219e7dd471741d1afa45d79be
Jacob Nuno <jacobcnda@gmail.com>
2018-12-05 13:22:48 -0600

    progress

--- app/js/api/users.js
+++ app/js/api/users.js
@@ -3,7 +3,8 @@
 class Users {
   constructor() {
     // const this.apiUrl = 'ec2-13-58-51-216.us-east-2.compute.amazonaws.com:3000';
-    this.apiUrl = 'ec2-13-58-51-216.us-east-2.compute.amazonaws.com:3000';
+    this.apiUrl = 'localhost:3000';
+    // this.apiUrl = 'ec2-13-58-51-216.us-east-2.compute.amazonaws.com:3000';
 
     // obtener la cookie
     // document.cookie
@@ -13,48 +14,87 @@ class Users {
     // this.headers = new Headers();
   }
 
-  static async login() {
-    return await fetch(`${apiUrl}/users/login`, {
+  static async login(form) {
+    const email = form.getElementsById('email').value;
+    const pass = form.getElementsById('password').value;
+    await fetch(`${this.apiUrl}/users/login`, {
       method: 'POST',
       headers: {
         // 'Content-Type': 'application/x-www-form-urlencoded'
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        email: 'ceoe1996@hotmail.com',
-        password: 1234
-      }),
-    }).then(function(response) {
-
-      // this.headers.set('Authorization', 'Bearer' + this.token);
-      // return response.json();
-      console.log(response.json());
-    });
-
-  }
-
-  static async getAll() {
-    return await fetch(`${this.apiUrl}/${this.endpoint}/asdfasd`, {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer' + this.token,
-        "Content-Type": "application/json"
-      }
-    }).then(function(response) {
-      return response.json();
-    });
-  }
-
-  static async create(form) {
-    data = await fetch(`${this.apiUrl}/${this.endpoint}`, {
-      method: 'post',
-      body: new FormData(form)
-    }).then(function(response){
-
+      body: {
+        email,
+        password: pass,
+      },
     })
-
-    document.cookie = 'credentials' + "=" + data.token + ";path=/;expires=" + d.toGMTString();
+      .then((response) => {
+        console.log('response: ', response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log('data: ', data);
+      })
+      .catch(err => console.log('err', err));
   }
+
+  // static async getAll() {
+  //   return await fetch(`${this.apiUrl}/${this.endpoint}/asdfasd`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Authorization': 'Bearer' + this.token,
+  //       "Content-Type": "application/json"
+  //     }
+  //   }).then(function(response) {
+  //     return response.json();
+  //   });
+  // }
+  //
+  // static async create(form) {
+  //   data = await fetch(`${this.apiUrl}/${this.endpoint}`, {
+  //     method: 'post',
+  //     body: new FormData(form)
+  //   }).then(function(response){
+  //
+  //   })
+  //
+  //   document.cookie = 'credentials' + "=" + data.token + ";path=/;expires=" + d.toGMTString();
+  // }
+  //
+  // static async userEdit() {
+  //     const url = window.location;
+  //     const id = url.toString().substring(url.toString().lastIndexOf('=') + 1);
+  //     console.log('id: ', id);
+  //     fetch(`${apiUrl}/users/${id}`, {
+  //     method: 'GET',
+  //     mode: 'cors',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Authorization: `Bearer ${token}`,
+  //     }
+  //   })
+  //     .then(function(response) {
+  //       console.log('response: ', response);
+  //       return response.json();
+  //     })
+  //     .then(function(data) {
+  //       console.log('data: ', data);
+  //       let username =
+  //       // const tableBody = document.getElementsByTagName('tbody')[0];
+  //       // for (i = 0; i < data.data.length; i++) {
+  //       //   let tr = document.createElement('tr');
+  //       //   let td = document.createElement('td');
+  //       //   td.appendChild(document.createTextNode(data.data[i]['id']));
+  //       //   tr.appendChild(td);
+  //       //   td = document.createElement('td');
+  //       //   td.appendChild(document.createTextNode(data.data[i]['name']));
+  //       //   tr.appendChild(td);
+  //       //   td = document.createElement('td');
+  //       //   td.appendChild(document.createTextNode(data.data[i]['email']));
+  //
+  //     })
+  //     .catch(err => console.log('err', err));
+  // }
 }
 
 module.exports = new Users();
