d0b0c65e014ca7f7bcc5b362959ebc67e1865e73
Carlos Orozco <ceoe1996@hotmail.com>
2018-12-11 07:36:25 -0600

    Solving Appointments

--- app/js/api/appointment.js
+++ app/js/api/appointment.js
@@ -5,17 +5,6 @@ class Appointment {
         let status;
         let user = Cookies.getCookie('user-id');
         let token = Cookies.getCookie('session-token');
-        console.log(user, token);
-
-        const body = {
-            serviceID: form.querySelector('select[id="txtService"]').value,
-            userID: user,
-            date: form.querySelector('input[id="txtDate"]').value + ' ' + form.querySelector('input[id="txtHour"]').value,
-            placeID: '1',
-            status: 'scheduled',
-        }
-
-        console.log(body);
 
         fetch(`https://pesoysalud.herokuapp.com/appointments/`, {
             method: 'POST',
@@ -30,6 +19,46 @@ class Appointment {
                 date: form.querySelector('input[id="txtDate"]').value + ' ' + form.querySelector('input[id="txtHour"]').value,
                 placeID: '1',
                 status: 'scheduled',
+            }),
+        })
+        .then((response) => {
+            console.log('response =', response);
+            status = response.status;
+            return response.json();
+          })
+        .then(function(data) {
+            console.log('data = ', data);
+            alert(data.message);
+          })
+        .catch(function(err) {
+          console.log('err', err);
+        });
+    }
+
+    static edit(form) {
+        let status;
+        let user = Cookies.getCookie('user-id');
+        let token = Cookies.getCookie('session-token');
+
+        const url = window.location;
+        const id = url.toString().substring(url.toString().lastIndexOf('=') + 1);
+        console.log(id);
+
+        const body = {
+            status: form.querySelector('select[id="txtStatus"]').value,
+        }
+
+        console.log(body);
+
+        fetch(`https://pesoysalud.herokuapp.com/appointments/${id}`, {
+            method: 'PUT',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `bearer ${token}`,
+            },
+            body: JSON.stringify({
+                status: form.querySelector('select[id="txtStatus"]').value,
             }),
         })
         .then((response) => {
